c1c2c6b22de07d96fe673195d1905d69bd3ebb35
retwanpro-cyber <retwan.pro@gmail.com>
2026-09-21 01:55:30 +0300

    Refactor dependency repositories in settings.gradle.kts
    Updated repository configuration for dependency resolution.

--- NovaChat/settings.gradle.kts
+++ NovaChat/settings.gradle.kts
@@ -5,17 +5,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = java.net.URI("https://jitpack.io") }
-    }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 rootProject.name = "NovaChat"
 include(":app")
